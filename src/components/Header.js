108d83b821ce1b91bbfc69929f3ee15398513dbb
s64park <s64park@uwaterloo.ca>
2017-01-29 21:14:22 -0500

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,7 +41,7 @@ class Header extends React.Component {
             <div>
                 <nav>
                     <div className="nav-wrapper blue darken-1">
-                        <Link to="/" className="brand-logo center">GymInst Forum</Link>
+                        <Link to="/" className="brand-logo center">MINIFORUM</Link>
 
                         <ul>
                             <li><a onClick={this.toggleSearch}><i className="material-icons">search</i></a></li>
